b9a5d873a5a7a0e3147c5e18db9c96587e4dee09
musaffer <168645050+musaffer@users.noreply.github.com>
2024-11-03 13:07:45 +0300

    Create core_init.h

---
+++ core_init.h
@@ -0,0 +1,13 @@
+// Core/Inc/core_init.h
+#ifndef INC_CORE_INIT_H_
+#define INC_CORE_INIT_H_
+
+#include "stm32f4xx_hal.h"
+#include "usbd_core.h"
+#include "usbd_desc.h"
+#include "usbd_cdc.h"
+#include "usbd_cdc_if.h"
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+#endif /* INC_CORE_INIT_H_ */
